b480996e4ae7f6d7f5f21ea63b17aa3bfef7a7f4
Celal Çağlar Öztürk <celalcaglar34@gmail.com>
2026-03-27 20:02:51 +0300

    1

--- app/src/main/java/eu/kanade/tachiyomi/util/LocalHttpServerService.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/LocalHttpServerService.kt
@@ -79,7 +79,11 @@ class LocalHttpServerService : Service() {
             .setOngoing(true)
             .build()
 
-        startForeground(NOTIFICATION_ID, notification)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            startForeground(NOTIFICATION_ID, notification, android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK)
+        } else {
+            startForeground(NOTIFICATION_ID, notification)
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
